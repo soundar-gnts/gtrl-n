38e2a8a94f40e05e9914ed84579b5bd04a40f00b
soundar-gnts <soundar@gnts.in>
2015-10-03 19:00:49 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,5 @@ var m_user_service = require('../services/m_user_service.js');
 
 module.exports = function(app, server){
 	app.post('/signup', m_user_service.signup);
-	app.post('/addUser', m_user_service.addUser);
+	
 }
